5de046f83633ffc13bbd5e1cf0e815032322577c
Josh Brunner <joshuaebrunner@gmail.com>
2017-05-01 07:46:15 -0700

    Initial commit

---
+++ code/LoginScreen/LoginScreen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoginScreen
+//
+//  Created by Josh Brunner on 5/1/17.
+//  Copyright © 2017 Helpful Human. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
